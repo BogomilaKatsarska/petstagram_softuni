de9ff97d04081d9a500a2dbe735a4eb822784df7
Bogomila <bkatsarska@abv.bg>
2022-11-07 17:24:51 +0200

    Update Workshop 2 - models.py and admin.py

--- petstagram_softuni/pets/admin.py
+++ petstagram_softuni/pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from petstagram_softuni.pets.models import Pet
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    pass
--- petstagram_softuni/settings.py
+++ petstagram_softuni/settings.py
@@ -1,3 +1,4 @@
+# 37:14 Workshop II
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,13 +57,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'petstagram_softuni.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Bogi1995',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
